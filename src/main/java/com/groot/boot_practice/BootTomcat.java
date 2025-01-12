a53cb6f3a3aa342c312f213e49d197f2692e2bf2
Juhyeong Ko <dury.ko@gmail.com>
2025-01-12 16:19:37 +0900

    feat: Hello Controller using spring container

--- src/main/java/com/groot/boot_practice/BootTomcat.java
+++ src/main/java/com/groot/boot_practice/BootTomcat.java
@@ -1,12 +1,12 @@
 package com.groot.boot_practice;
 
-import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
+import org.springframework.context.support.GenericApplicationContext;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
@@ -16,15 +16,18 @@ import java.io.IOException;
 public class BootTomcat {
     
     public static void main(String[] args) {
+        // Spring Container = Application Context 생성
+        GenericApplicationContext applicationContext = new GenericApplicationContext();
+        applicationContext.registerBean(HelloController.class);
+        applicationContext.refresh();
+        
         ServletWebServerFactory servletWebServerFactory = new TomcatServletWebServerFactory();
         WebServer webServer = servletWebServerFactory.getWebServer(servletContext -> {
-            HelloController helloController = new HelloController();
-            
             servletContext.addServlet("hello", new HttpServlet() {
                 @Override
-                protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException,
-                        IOException {
+                protected void service(HttpServletRequest req, HttpServletResponse resp) throws IOException {
                     if (req.getRequestURI().equals("/hello") && req.getMethod().equals(HttpMethod.GET.name())) {
+                        HelloController helloController = applicationContext.getBean(HelloController.class);
                         String ret = helloController.hello(req.getParameter("name"));
                         
                         resp.setContentType(MediaType.TEXT_PLAIN_VALUE);
